61f3063eff4d097e48e784b5c5afd068de7da757
ajc <ajc@admin-PC>
2018-02-13 10:54:31 +0100

    hhhh

--- testMavenGIT/src/main/java/testMaven/Testjava.java
+++ testMavenGIT/src/main/java/testMaven/Testjava.java
@@ -5,7 +5,7 @@ public class Testjava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello brave new worldwar II");
+		System.out.println("hello brave new worldwar");
 		
 	
 		
