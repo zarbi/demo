aeca8b6fe2f0efdfccbb5ea1fa9086d03195a75f
ajc <ajc@admin-PC>
2018-02-13 10:22:41 +0100

    First test

---
+++ testMavenGIT/src/main/java/testMaven/Testjava.java
@@ -0,0 +1,20 @@
+package testMaven;
+
+import metier.Commande;
+import packpersonne.personne;
+
+public class Testjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		
+	
+		
+
+	Commande c = new Commande("toto","tutu") ;
+	
+	System.out.println(c);
+
+	}
+}
